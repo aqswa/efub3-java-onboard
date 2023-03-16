ce7abd72851c5e3bcca0af0f83cd5043b9edc8bc
aqswa <joayoung3@gmail.com>
2023-03-16 20:04:04 +0900

    problem2

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -5,7 +5,17 @@ import java.util.List;
 
 public class Problem2 {
     public static Integer[] solution(Integer[][] flipCards){
-        Integer[] answer = {};
+        Integer[] answer = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for(int i=0; i<flipCards.length; i++){
+            int s = flipCards[i][0];
+            int e = flipCards[i][1];
+            int mid = (s-e)/2;
+            for(int j=0; j<=mid; j++){
+                int temp = anser[s+j];
+                answer[s+j] = answer[e-j];
+                answer[e-j] = temp;
+            }
+        }
         return answer;
     }
 }
