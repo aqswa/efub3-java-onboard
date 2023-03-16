11736943889b20f9d17204dddd35cb91fca0c0df
aqswa <joayoung3@gmail.com>
2023-03-16 20:33:36 +0900

    problem5

--- src/main/java/onboard/Problem5.java
+++ src/main/java/onboard/Problem5.java
@@ -3,6 +3,12 @@ package onboard;
 public class Problem5 {
     public static Integer solution(Integer N, Integer K, Integer[] values){
         Integer answer = 0;
+        for(int i=values.length; i>=0; i--){
+            if(K/values[i] > 0){
+                answer += K/values[i];
+                K %= values[i];
+            }
+        }
         return answer;
     }
 }
