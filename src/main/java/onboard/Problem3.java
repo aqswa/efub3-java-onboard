9682d76bd4e43d74c9afecb7753e51414805378c
aqswa <joayoung3@gmail.com>
2023-03-16 20:10:51 +0900

    problem3

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -1,11 +1,16 @@
 package onboard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+        Arrays.sort(numbers);
+        Integer mean = (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]) / 5;
+        answer.add(mean);
+        answer.add(numbers[2]);
         return answer;
     }
 }
